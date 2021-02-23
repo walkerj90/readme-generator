ac4ea3f7550660349168bd30acb299d87a693d19
Justin <walkerj90@icloud.com>
2021-02-23 14:24:44 -0800

    Restarted markdown file took the template from markdown and added it to index instead. as well as wrote a function to create a readme file. Still cant get the license to show up.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown.js');
 const fs = require('fs');
 //Create an array of questions for user input
 const questions = [
@@ -28,18 +27,18 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'InstallInstructions',
+        name: 'installation',
         message: 'How should the user install this project?'
     },
     {
-        type: 'rawlist',
+        type: 'list',
         name: 'license',
         message: 'What type of license does this fall under?',
         choices: [
             'MIT License',
             'Apache License 2.0',
             'GNU GPLv3 License',
-            'ISC License',
+            'Other',
         ],
     },
     {
@@ -48,7 +47,7 @@ const questions = [
         name: 'UserName',
         default: 'walkerj90',
         validate: function (answer) {
-            if (answer.length < 1) {
+            if (answer.length = 0) {
                 return console.log("A valid GitHub username is required.");
             }
             return true;
@@ -78,21 +77,54 @@ const questions = [
 ]
 
 //Create a function to write README file
-function writeToFile(filename, data) {
-    fs.writeFileSync(fileName, data, err => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Your README.md file has been generated sucessfully.")
-    });
-}
+function writeToFile(response) {
+    console.log(response);
+    return `
+    
+    # ${response.title}
+    ![License](https://img.shields.io/badge/license-${response.license}-blue.svg)
+    
+    ## Project Description
+    ${response.description}
+    
+    ## Table of Contents
+    [Project Description](#Project Description)
+    [Installation](#Installation)
+    [Usage](#Usage)
+    [Contributions](#Contributions)
+    [Licensing](#Licensing)
+    [Testing](#Testing)
+    [Contact](#Questions)
+    
+    ## Installation
+    ${response.installation}
+    
+    ## Usage 
+    ${response.usage}
+    
+    ## Licensing (#Licensing)
+    ${response.license}
+    
+    ## Contributing
+    ${response.contributions}
+    
+    ## Testing
+    ${response.test}
+    
+    ## Contact
+    If you wish to contact me about this project with any comments or questions, you can reach me at my GitHub or Email.
+    Link to GitHub: Github.com/${response.UserName}
+    Email: ${response.email}`
+};
+
 
 //Create a function to initialize app
 function init() {
-    inquirer.prompt(questions)
-        .then(function (data) {
-            writeToFile("README.md", generateMarkdown(data));
-        });
+    inquirer.prompt(questions).then((response) => {
+        fs.writeFileSync(`${response.title}-README.MD`, writeToFile(response), (err) => {
+            err ? console.log(err) : console.log(`You have sucessfully generated the README for ${response.title}.`)
+        })
+    })
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,54 +11,9 @@ function renderLicenseLink(license) { }
 function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
-function markdown(data) {
+function generateMarkdown(data) {
   return `# ${data.title}
-[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/${data.UserName}/${data.Title})
-
-# Description
-${data.Description}
-
-# Table of Contents
-
-* Installation (#installation)
-
-* Usage (#usage)
-
-* License (#license)
-
-* Contributing (#contributing)
-
-* Tests (#tests)
-
-* Questions (#questions)
-
-* Acknowledgements (#acknowledgements)
-
-# Installation Instructions
-
-You must have the following dependencies installed prior to running this application correctly. ${data.Installation}
-
-# Usage
-
-To use this application you must first see the following. ${data.Usage}
-
-# Licensing
-
-This project uses the following licenses. ${data.License}
-
-# Contributions
-
-${data.Contributor}
-
-#Testing
-
-In order to test this application you must use the following commands and/or steps. ${data.test}
-
-#Contact information
-
-If you have any further questions about the following app please contact ${data.Username} at (https://github.com/${data.UserName})
-or at the email ${data.email}
 `;
 }
 
-module.exports = markdown;
+module.exports = generateMarkdown;
